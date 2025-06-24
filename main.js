4ba7f69763aac5a2a09942a3073f2f054654310b
nanachiki <nanachiki256@gmail.com>
2025-06-24 21:52:38 +0900

    Add random wallpaper button to display a random motivational wallpaper on click

--- main.js
+++ main.js
@@ -38,23 +38,27 @@ const wallpapers = [
     "https://cdn.pixabay.com/photo/2020/06/12/03/06/magnifying-glass-5288877__340.jpg", 
     "start",
     "end"
-  ),
+  ).createWallpaper(),
   new Wallpaper(
     "The scientist discovers a new type of material or energy and the engineer discovers a new use for it. - Gordon Lindsay Glegg",
     "007bff",
     "https://cdn.pixabay.com/photo/2018/02/23/04/38/laptop-3174729_1280.jpg", 
     "center", 
     "start"
-  ),
+  ).createWallpaper(),
   new Wallpaper(
     "Scientists study the world as it is, engineers create the world that never has been. - Theodore von Karman", 
     "ecf0f1", 
     "https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_1280.jpg", 
     "center", 
     "center"
-  )
-] 
+  ).createWallpaper()
+];
 
-wallpapers.forEach(wallpaper => {
-  mainElement.append(wallpaper.createWallpaper());
+mainElement.append(wallpapers[0]);
+
+document.getElementById('change-wallpaper').addEventListener('click', () => {
+  mainElement.innerHTML = ''; // Clear the main wrapper when the button is clicked
+  const randomIndex = Math.floor(Math.random() * wallpapers.length);
+  mainElement.append(wallpapers[randomIndex]); // Append a random wallpaper
 });
